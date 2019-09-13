d02dc90757f3b4a2b587619c9771bff05927e267
Kobi Cummings <kobicummings@gmail.com>
2019-09-13 09:20:39 -0700

    made changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 console.log("TEST");
-alert("SURPRISED THIS WORKS... YA, SAME HERE!")
 
 
+$(document).ready(function() {
 var firebaseConfig = {
     apiKey: "AIzaSyBf39CL-NvgZHlCfWC5YgQcCKQZL110yQc",
     authDomain: "kc-cbc-activities.firebaseapp.com",
@@ -11,29 +11,61 @@ var firebaseConfig = {
     messagingSenderId: "534272082764",
     appId: "1:534272082764:web:166fc7f479c2caabbd45e9"
   };
-  // Initialize Firebase
+  
   firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+  
+var name;
+var destination;
+var firstTime;
+var frequency = 0;
+
 
   $("#formID").on("submit", function (event) {
       event.preventDefault();
+
     var name = $("#trainName").val().trim();
     var destination = $("#trainDestination").val().trim();
     var firstTime = $("#trainTime").val().trim();
     var frequency = $("#frequency").val().trim();
 
-
 database.ref().push({
-    name: name,
-    destination: destination,
-    firstTime: firstTime,
-    frequency: frequency
+  name: name,
+  destination: destination,
+  firstTime: firstTime,
+  frequency: frequency,
+  dateAdded: firebase.database.serverValue.TIMESTAMP
 });
 
-$("#trainName").val("");
-$("#trainDestination").val("");
-$("#trainTime").val("");
-$("#frequency").val("");
+$("form")[0].reset();
 
-return false;
+});
 
-  });
+database.ref().on("child_added", function(childSnapshot) {
+
+var nextArr;
+var minAway;
+
+var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+
+var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+var remainder = diffTime % childSnapshot.val().frequency;
+
+var minAway = childSnapshot.val().frequency - remainder;
+
+var nextTrain = moment().add(minAway, "minutes");
+nextTrain = moment(nextTrain).format("hh:mm");
+
+$("#tableContent").append("<tr><td>" + childSnapshot.val().name + 
+"</td><td>" + childSnapshot.val().destination +
+"</td><td" + childSnapshot.val().frequency +
+"</td><td>" + nextTrain +
+"</td><td>" + minAway + "</td><tr>");
+
+}, function(errorObject) {
+  console.log("Error fixed: " + errorObject.code);
+});
+
+
+});
